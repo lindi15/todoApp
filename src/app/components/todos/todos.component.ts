2db9ba4d1e2a1585b31d1089f1e54ea15f868f17
DA_learner <da_learner@DA-learners-MacBook-Air.local>
2021-12-20 14:35:33 +0200

    Initial commit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,42 +7,54 @@ import { Todo } from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
 
-  todos:Todo[];
+  todos: Todo[];
 
-  inputTodo:string ="";
+  inputTodo: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
     this.todos = []
-}
-
- toggleDone (id:number) {
- this.todos.map((v, i) => {
-  if (i ==id) v.completed = !v.completed;
-
-  return v;
-})
-}
-
-deleteTodo (id:number) {
-  this.todos = this.todos.filter((v, i) => i !== id);
-}
-
-addTodo () {
-  if(this.inputTodo==""){
-    return null;
-  }else{
-    this.todos.push({
-      content: this.inputTodo,
-      completed: false
-    });
   }
 
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    if (this.inputTodo == "") {
+      return null;
+    } else {
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false
+      });
+    }
+  }
+
+
+  updateTodo(id: number) {
+    let i = id;
+    if (i != null) {
+
+      console.log("value updated")
+    } else {
+      console.log("value not updated")
+    }
+
+  }
   
 
-  this.inputTodo= "";
+  // this.inputTodo = "";
 
 
-}
+
 }
